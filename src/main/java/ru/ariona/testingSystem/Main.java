bab8bc2b820c0990ff7ce209da487b2bc5a71964
СапоГ <katenochekspb@mail.ru>
2019-11-02 18:15:46 +0300

    init

--- src/main/java/ru/ariona/testingSystem/Main.java
+++ src/main/java/ru/ariona/testingSystem/Main.java
@@ -3,13 +3,10 @@ package ru.ariona.testingSystem;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.ariona.testingSystem.controller.TestController;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
         TestController controller = context.getBean(TestController.class);
         controller.startTest();
-
     }
 }
